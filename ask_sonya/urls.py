012e5fda7ae163f5e402331bdfadd25b5f58d51b
sprosonya <166607219+sprosonya@users.noreply.github.com>
2024-06-19 20:47:12 +0300

    Delete ask_sonya directory

--- ask_sonya/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from app import views
-
-urlpatterns = [
-    path('', include('app.urls')),
-    path('admin/', admin.site.urls),
-]
