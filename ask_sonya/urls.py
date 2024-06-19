8905ca1937a2f43b1ee91a759e721d20eb57959b
sprosonya <166607219+sprosonya@users.noreply.github.com>
2024-06-19 20:47:23 +0300

    Add files via upload

---
+++ ask_sonya/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from app import views
+
+urlpatterns = [
+    path('', include('app.urls')),
+    path('admin/', admin.site.urls),
+]
